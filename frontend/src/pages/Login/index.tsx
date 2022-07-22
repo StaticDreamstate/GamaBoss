226ab89d11528c649b3356a3ff300f25b8345f27
pradojvictor <joaovictorred@hotmail.com>
2022-07-22 20:56:51 -0300

    home, login, componente box

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,5 +1,18 @@
+import logo from "../../assets/hometest.png"
+import Box from "../../components/BoxHome";
+
 export default function Login(){
   return (
-    <main>Página de Login</main>
+    <main className="pt-10 grid justify-items-stretc">
+            
+    <img src={logo} alt="imagem de" className="justify-self-center h-60 rounded-2xl border-solid border-2 border-slate-200" />
+
+    <h2 className='justify-self-center pt-10'>Lorem ipsum dolor sit amet.</h2>
+    <p className=" justify-self-center pt-2 pb-5 px-10">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+   
+    <button className=" justify-self-center mt-10 h-10 w-40 rounded-md border-solid border-2 border-slate-200">teste</button>
+   
+
+</main>
   )
 }
