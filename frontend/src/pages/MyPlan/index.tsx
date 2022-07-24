489c5fe25580303620f86e04384a5fc30f63b63c
Juliana Amancio <jhullyjoviano@gmail.com>
2022-07-23 23:25:31 -0300

    adicionando component accordion

--- frontend/src/pages/MyPlan/index.tsx
+++ frontend/src/pages/MyPlan/index.tsx
@@ -1,5 +1,15 @@
+import Accordion from "../../components/Accordion";
+
 export default function MyPlan(){
   return (
-    <main>Página de Meu plano</main>
+    
+    <main>Página de Meu plano
+     <Accordion titleAccordion={"What is Flowbite?"} contentAccordion={"Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,"} />
+     <Accordion titleAccordion={"Teste2?"} contentAccordion={"F is an open-source library of interactive components built on top of Tailwind CSS including buttons,"} />
+     <Accordion titleAccordion={"Teste 3"} contentAccordion={" an open-source library of interactive components built on top of Tailwind CSS including buttons,"} />
+
+    </main>
+    
+    
   )
 }
