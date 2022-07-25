95f9b6a0a37b0b186ba12a194231e942748dd813
Rafael Sangali <rafaelsangali@outlook.com>
2022-07-25 11:15:46 -0300

    alteração no header, e importações de icones

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,1 +1,11 @@
-// importar imagens e exportar como objeto
+import iconMenu from "./icons/iconMenu.svg"
+import iconClose from "./icons/iconClose.svg"
+import iconArrowDown from "./icons/iconArrowDown.svg"
+
+export const icons = {
+  iconMenu: iconMenu,
+  iconClose: iconClose,
+  iconArrowDown: iconArrowDown,
+}
+
+export const images = {}
--- frontend/src/components/Accordion/index.tsx
+++ frontend/src/components/Accordion/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { icons } from "../../assets";
 
 interface IaccordionProps {
   titleAccordion: string;
@@ -30,20 +31,13 @@ export default function Accordion(props: IaccordionProps) {
               className={`flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200`}
             >
               <span>{props.titleAccordion}</span>
-              <svg
+              <img
                 className={`w-6 h-6  shrink-0 ${
                   activeAccordion.state == true ? "rotate-180" : ""
                 }`}
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+                src={icons.iconArrowDown}
+                alt="Icone de seta"
+              />
             </button>
           </h2>
           <div
