821e34e90cd8b9f3b1ca77d20e324962bc7ab47a
StaticDreamstate <lonely_loonie@protonmail.com>
2022-07-25 23:59:11 -0300

    Deploy da API no heroku

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,6 +1,11 @@
 import Connection from "./Connection";
 import ENV from "../infra/config/env";
 
+
+if (ENV.NODE_ENV !== "production") {
+    const mongoDB = new Connection(`${ENV.MONGODB_URL}`);
+  }
+  
 const mongoDB = new Connection(`mongodb://${ENV.DB_HOST}:${ENV.DB_PORT}/${ENV.DB_NAME}`);
 
 export { mongoDB };
