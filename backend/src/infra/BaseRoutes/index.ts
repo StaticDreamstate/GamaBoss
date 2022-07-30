3ec3d4377bb316159075fa99b42eb134b2abdda0
StaticDreamstate <lonely_loonie@protonmail.com>
2022-07-30 11:23:22 -0300

    Rotas de Pet finalizadas.

--- backend/src/infra/BaseRoutes/index.ts
+++ backend/src/infra/BaseRoutes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRoutes from "../../modules/Cliente/routes";
 import authRoutes from "../../modules/Auth/routes";
+import petRoutes from "../../modules/Pet/routes";
 
 const routes = Router();
 
 routes.use(userRoutes);
 routes.use(authRoutes);
+routes.use(petRoutes);
 
 export default routes;
