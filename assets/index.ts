ee87e2bcf98f375f5e4b9b69098a3171792ebe1a
MATEUS FARIAS SANTOS <95185745+tereus100@users.noreply.github.com>
2022-07-29 23:29:32 -0300

    Commit do footer opções de pagamento.
    commit final do footer.

---
+++ assets/index.ts
@@ -0,0 +1,26 @@
+import facebookIcon from "./icons/facebookIcon.png";
+import instagramIcon from "./icons/instagramIcon.png";
+import linkedinIcon from "./icons/linkedinIcon.png";
+import whatsappIcon from "./icons/whatsappIcon.png";
+import iconCircle from "./icons/iconCircle.png";
+import iconMenu from "./icons/iconMenu.svg";
+import iconClose from "./icons/iconClose.svg";
+import imagePets from "./images/imagePets.svg";
+import iconEdit from "./icons/edit.svg"
+import iconCamera from "./icons/iconCamera.svg"
+
+export const images = {
+  imagePets,
+};
+
+export const icons = {
+  iconMenu,
+  iconClose,
+  iconCircle,
+  facebookIcon,
+  instagramIcon,
+  linkedinIcon,
+  whatsappIcon,
+  iconEdit,
+  iconCamera,
+};
