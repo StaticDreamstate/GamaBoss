29ca8dadb1d35c2c1eb83c5e379d9fbf8abaf074
Rafael Sangali <rafaelsangali@outlook.com>
2022-08-01 15:03:33 -0300

    ajeitando imagens

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,27 +1,26 @@
-//icons
 import facebookIcon from "./icons/facebookIcon.png";
 import instagramIcon from "./icons/instagramIcon.png";
 import linkedinIcon from "./icons/linkedinIcon.png";
 import whatsappIcon from "./icons/whatsappIcon.png";
 import iconCircle from "./icons/iconCircle+.png";
-import iconNegative from "./icons/iconCircle-.png"
+import iconNegative from "./icons/iconCircle-.png";
 import iconMenu from "./icons/iconMenu.svg";
 import iconClose from "./icons/iconClose.svg";
-import iconEdit from "./icons/edit.svg"
-import iconCamera from "./icons/iconCamera.svg"
-import adicionarPet from "./images/adicionarPet.png"
-import meuPlano from "./images/meuPlano.png"
-import monitorCard from "./images/monitorCard.png"
-import pataPet from "./images/pataPet.png"
-import dog1 from "./images/dog1.png"
-import dog2 from "./images/dog2.png"
-import dog3 from "./images/dog3.png"
-import avatarBar from "./icons/avatarBar.svg"
-import iconCheck from "./icons/iconCheck.png"
-import iconArromRight from "./icons/iconArrowRight.png.png"
+import iconEdit from "./icons/edit.svg";
+import iconCamera from "./icons/iconCamera.svg";
+import adicionarPet from "./images/adicionarPet.png";
+import meuPlano from "./images/meuPlano.png";
+import monitorCard from "./images/monitorCard.png";
+import pataPet from "./images/pataPet.png";
+import dog1 from "./images/dog1.png";
+import dog2 from "./images/dog2.png";
+import dog3 from "./images/dog3.png";
+import avatarBar from "./icons/avatarBar.svg";
+import iconCheck from "./icons/iconCheck.png";
+import iconArromRight from "./icons/iconArrowRight.png.png";
 import imagePets from "./images/imagePets.svg";
 import DogHome from "./images/DogHome.png";
-
+import petAvatar from "./images/petAvatar.png";
 
 export const images = {
   imagePets,
@@ -49,5 +48,5 @@ export const icons = {
   iconCamera,
   avatarBar,
   iconCheck,
-  iconArromRight
+  iconArromRight,
 };
