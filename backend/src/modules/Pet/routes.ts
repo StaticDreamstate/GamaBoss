c2343a64ae8a2833e286aef092c788b6c7c3319d
StaticDreamstate <lonely_loonie@protonmail.com>
2022-08-02 03:39:10 -0300

    Refatoração do módulo Pets

--- backend/src/modules/Pet/routes.ts
+++ backend/src/modules/Pet/routes.ts
@@ -7,7 +7,7 @@ const routes = Router();
 routes.post("/cadastrar-pet", imgUpload.single("avatar"), controller.createPet);
 routes.get("/listar-pets/:dono", controller.getPets);
 routes.get("/listar-pet/:id", controller.getPetById);
-routes.put("/editar-pet/:id", imgUpload.single("avatar"), controller.editPet);
+routes.put("/editar-pet/:dono/:id", imgUpload.single("avatar"), controller.editPet);
 routes.delete("/deletar-pet/:id", controller.deletePet);
 
 export default routes;
