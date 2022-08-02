2b4a89b1c61de5d5e52df2b62b7065b9f3614015
Wallace Nascimento <wallacediogo10@hotmail.com>
2022-08-02 16:06:10 -0300

    feat: concluded profile page

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -21,6 +21,15 @@ import iconArromRight from "./icons/iconArrowRight.png.png";
 import imagePets from "./images/imagePets.svg";
 import DogHome from "./images/DogHome.png";
 import petAvatar from "./images/petAvatar.png";
+import calendar_today from "./icons/calendar_today.png";
+import edit from "./icons/edit.png";
+import medical_services from "./icons/medical_services.png";
+import vacine from "./icons/vacine.png";
+import consult from "./icons/consult.png";
+import medical_service_card from "./images/medical_service_card.png";
+import vaccines from "./images/vaccines.png";
+import healing from "./images/healing.png";
+import calendar_month from "./images/calendar_month.png";
 
 export const images = {
   imagePets,
@@ -33,6 +42,10 @@ export const images = {
   dog2,
   dog3,
   petAvatar,
+  calendar_month,
+  healing,
+  vaccines,
+  medical_service_card,
 };
 
 export const icons = {
@@ -49,4 +62,9 @@ export const icons = {
   avatarBar,
   iconCheck,
   iconArromRight,
+  calendar_today,
+  vacine,
+  medical_services,
+  edit,
+  consult,
 };
