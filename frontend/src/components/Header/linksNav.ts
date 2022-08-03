ec6408de650a8ec9f9af646d66b87161309b63b1
Wallace Nascimento <wallacediogo10@hotmail.com>
2022-08-03 11:37:43 -0300

    feat: navbar header

--- frontend/src/components/Header/linksNav.ts
+++ frontend/src/components/Header/linksNav.ts
@@ -1,10 +1,5 @@
 export const linksNav = [
   { name: "Entrar/Cadastrar", redirect: "/" },
-  
-  { name: "Quem Somos", redirect: "" },
-  { name: "Clínicas parceiras", redirect: "" },
-  { name: "Nossos planos", redirect: "/meu-plano" },
-  { name: "Nosso blog", redirect: "" },
-  { name: "Perfil pet", redirect: "/perfil" },
-  { name: "Meu plano", redirect: "/meu-plano" },
+  { name: "Perfil pet", redirect: "perfil" },
+  { name: "Meu Perfil", redirect: "" },
 ];
