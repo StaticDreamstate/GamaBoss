dbf7db75669b52fcec72b6303745b390cd7693da
Rafael Sangali <rafaelsangali@outlook.com>
2022-08-03 15:22:08 -0300

    ajeitando a home

--- frontend/src/components/MenuItemsProfile/index.tsx
+++ frontend/src/components/MenuItemsProfile/index.tsx
@@ -5,8 +5,8 @@ interface ItemsProps {
   
   export default function MenuItemsProfile(props: ItemsProps) {
     return (
-      <div className="flex justify-start mt-3">
-        <img src={props.image} />
+      <div className="flex justify-start items-center">
+        <img className="bg-card p-1 rounded-lg"  src={props.image} />
         <label className="ml-3 text-gray-800" htmlFor="flexCheckDefault">
           {props.textContent}
         </label>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,43 +1,23 @@
-import Box from "../../components/BoxHome";
 import { images } from "../../assets";
-
+import CardPets from "../../components/CardPets";
 
 export default function Home() {
   return (
     <>
-      <main className="pt-10 grid justify-items-stretc">
-        <img src={"logo"} alt="imagem de pets" className="" />
-
-        <h2 className=" pt-10 px-20">Lorem ipsum dolor sit amet.</h2>
-        <p className="pt-2 pb-5 px-20">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, esse
-          nesciunt. Officiis fugit u.
+      <main className="grid justify-items-stretc">
+        <img src={images.DogHome} alt="imagem de pets" className="w-screen" />
+        <h2 className=" pt-10 px-6 font-600 text-mobile-size-24px">
+          Olá, <strong className="text-primary-blue-1">Alessandra!</strong>
+        </h2>
+        <p className="px-6 text-mobile-size-16px">
+          O que você deseja fazer hoje?
         </p>
-        <div className="px-20">
-          <Box></Box>
-          <Box></Box>
-          <Box></Box>
-        </div><source media="(s)" srcSet="" />
-
-         <div className="flex justify-center gap-6 flex-wrap w-auto items-center ">
-          <div className="bg-[#EDF7FF] w-[148px] h-[113px] pt-5 text-center text-[#027BE3]">
-            <img src={images.pataPet} className="pl-12" />
-           <button className=""> Perfil Pet</button>
-           </div>
-          <div className="bg-[#EDF7FF] w-[148px] h-[113px] pt-5 text-center text-[#027BE3]">
-          <img src={images.monitorCard} className="pl-12" />
-           <button className=""> Nosso blog</button>
-           </div>
-          <div className="bg-[#EDF7FF]  w-[148px] h-[113px] pt-5 text-center text-[#027BE3]">
-          <img src={images.adicionarPet} className="pl-12" />
-           <button className=""> Adicionar Pet</button>
-           </div>
-          <div className="bg-[#EDF7FF] w-[148px] h-[113px] pt-5 text-center text-[#027BE3]">
-          <img src={images.meuPlano} className="pl-12" />
-           <button className=""> Meu plano</button>
-           </div>
-         </div>
-      
+        <div className="flex justify-center gap-6 flex-wrap w-auto items-center ">
+          <CardPets imagePet={images.pataPet} texto="Perfil Pet" redirect="#" />
+          <CardPets imagePet={images.monitorCard} texto="Nosso blog" redirect="#" />
+          <CardPets imagePet={images.adicionarPet} texto="Adicionar Pet" redirect="#" />
+          <CardPets imagePet={images.meuPlano} texto="Meu Plano" redirect="#" /> 
+        </div>
       </main>
     </>
   );
