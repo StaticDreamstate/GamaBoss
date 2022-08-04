ecffd4682ff62653b5cc29493714acc4ed18418f
Rafael Sangali <rafaelsangali@outlook.com>
2022-08-04 14:17:28 -0300

    ajustes header cards amarelos

--- frontend/src/components/CardPets/index.tsx
+++ frontend/src/components/CardPets/index.tsx
@@ -3,13 +3,14 @@ interface ICarPetProps {
   imagePet: any;
   texto: string;
   redirect: string;
+  className?: string;
 }
 
 export default function CardPets(props: ICarPetProps) {
   return (
     <a
       href={props.redirect}
-      className="flex h-[120px] mb-4 rounded-lg border-solid border-0 bg-card"
+      className={`flex h-[120px] w-[311px] mb-4 rounded-lg border-solid border-0 bg-[#FAF6E6] ${props.className}`}
     >
       <img className="rounded-l-lg w-[130px]" src={props.imagePet} alt="" />
       <p className="mx-5 my-9 font-500 text-mobile-size-14px font-libre leading-[19.6px] ">
