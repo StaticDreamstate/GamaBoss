a9eb0142a6942715b3910e9e37c7e1aedaa3d9bf
Rafael Sangali <rafaelsangali@outlook.com>
2022-08-04 13:57:49 -0300

    ajustes nos cards azuls

--- frontend/src/components/CardBluePets/index.tsx
+++ frontend/src/components/CardBluePets/index.tsx
@@ -1,37 +1,20 @@
-
 interface ICarPetProps {
-    children?: JSX.Element;
-    imagePet: any;
-    texto: string;
-    redirect: string;
+  children?: JSX.Element;
+  imagePet: any;
+  texto: string;
+  redirect: string;
 }
 
 export default function CardPets(props: ICarPetProps) {
-    return (
-        <a href={props.redirect}
-            className="bg-[#EDF7FF] w-[148px] h-[113px] pt-5 text-center text-[#027BE3] rounded-2xl shadow-[4px_5px_10px_#393a3c0d]">
-
-            <img src={props.imagePet} className="pl-12" />
-            <button className="font-600 text-mobile-size-16px font-libre">{props.texto}</button>
-
-        </a>
-
-    );
+  return (
+    <a
+      href={props.redirect}
+      className="bg-card w-[144px] h-[113px] flex flex-col items-center justify-center gap-4 text-primary-blue-1 rounded-2xl shadow-[4px_5px_10px_#393a3c0d] hover:scale-105 transition-transform"
+    >
+      <img src={props.imagePet} width={40} />
+      <button className="font-600 text-mobile-size-16px font-libre">
+        {props.texto}
+      </button>
+    </a>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-//  <div className="flex justify-center gap-6 flex-wrap w-auto items-center ">
-//  <div className="bg-[#EDF7FF] w-[148px] h-[113px] pt-5 text-center text-[#027BE3]">
-//    <img src={images.pataPet} className="pl-12" />
-//    <button className=""> Perfil Pet</button>
-//  </div>
-// </div>
