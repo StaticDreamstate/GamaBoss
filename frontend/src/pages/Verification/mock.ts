45a08e1fccab0d143b9aecd3f5019a6104eb8d9a
Your Name <ffernandaa92@gmail.com>
2022-08-04 19:33:58 -0300

    ajustes botoes e mock

---
+++ frontend/src/pages/Verification/mock.ts
@@ -0,0 +1,6 @@
+export const mockVerification = { 
+    servico1:"Teleatendimento",
+    servico2:"Consulta em domicílio",
+    servico3:"Cobertura Nacional",
+    servico4:"Procedimentos Cirúrgicos"
+}
