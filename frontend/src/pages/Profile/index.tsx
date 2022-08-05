3bd3da69500c8e4db642cf46b64a7a2fdfc4a15c
Juliana Amancio <102419005+JulianaAmancio@users.noreply.github.com>
2022-08-05 03:01:08 -0300

    ajustes na tela meu perfil

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -11,14 +11,14 @@ export default function Profile() {
       <section className="mt-8">
         <PetAvatar className="w-max" />
       </section>
-      <section className="flex flex-col md:flex-row sm:my-10 md:flex-wrap lg:mx-[230px] ">
+      <section className=" pt-4 flex flex-col text-gray-1 md:flex-row sm:my-10 md:flex-wrap lg:mx-[230px] ">
         {mockPlansIcons.map((plan, index) => (
-          <div key={plan.title + index} className="py-3 px-6 w-[260px] border-b-2 md:border-none">
+          <div key={plan.title + index} className="py-5 px-6 w-full border-b-2 md:border-none">
             <MenuItemsProfile image={plan.image} textContent={plan.title} />
           </div>
         ))}
       </section>
-      <p className="pt-12 pb-9 pl-6 pr-10 border-t-2 ">
+      <p className="pt-12 pb-9 pl-6 pr-10 text-gray-1text-gray-1 ">
         Consulte a quantidade de serviços que você já utilizou
       </p>
       <section className="flex justify-center pt-2 gap-6 flex-wrap w-auto items-center mb-20">
